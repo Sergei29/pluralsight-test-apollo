7d878e603201e1b3f5fde5b983a06c3c45eeca70
Serge Basangovs <sergejs.basangovs@gmail.com>
2021-05-31 17:28:43 +0300

    added basic edit form

--- client/src/components/Stories/Stories.jsx
+++ client/src/components/Stories/Stories.jsx
@@ -1,10 +1,12 @@
-import React from "react";
-import { useQuery } from "@apollo/client";
+import React, { useState } from "react";
+import { useQuery, useMutation } from "@apollo/client";
 import { STORIES_QUERY } from "../../graphql/queries";
+import { EDIT_STORY_NAME } from "../../graphql/mutations";
 import Story from "../Story/Story";
 import { defaultMessages } from "../../constants";
 
 const Stories = () => {
+  const [objStories, setObjStories] = useState({});
   const { loading, error, data } = useQuery(STORIES_QUERY, {
     errorPolicy: "all",
   });
@@ -12,12 +14,21 @@ const Stories = () => {
   if (loading) return <p>Loading...</p>;
   // if (error) return <p>{defaultMessages.ERROR_LOADING_DATA}</p>;
 
+  const handleChangeName = (strId) => (objEvent) => {
+    objEvent.persist();
+    setObjStories((prevObjStories) => ({
+      ...prevObjStories,
+      [strId]: objEvent.target.value,
+    }));
+  };
+
   const stories = data?.stories?.map((story) => (
     <Story
       key={story.id}
       name={story.name}
       image={story.image}
       description={story.description}
+      handleChangeName={handleChangeName(story.id)}
     />
   ));
 
--- client/src/components/Story/Story.jsx
+++ client/src/components/Story/Story.jsx
@@ -1,17 +1,27 @@
 import React from "react";
 
-const Story = ({ image, name, description }) => {
+const Story = ({ image, name, description, handleChangeName }) => {
   return (
     <div className="col-lg-3 col-md-6">
-      <a href="http://some-link-to-resource">
-        <div className="crf-story--image">
-          <img alt={name} src={image} />
+      <div className="crf-story--image">
+        <img alt={name} src={image} />
+      </div>
+      <div className="crf-story--text">
+        <h3>
+          {name} <button className="btn btn-primary">Edit</button>
+        </h3>
+        <div className="form-group">
+          <input
+            type="text"
+            className="form-control"
+            onChange={handleChangeName}
+          />
+          <div>
+            <button className="btn btn-secondary">save</button>
+          </div>
         </div>
-        <div className="crf-story--text">
-          <h3>{name}</h3>
-          <div>{description}</div>
-        </div>
-      </a>
+        <div>{description}</div>
+      </div>
     </div>
   );
 };
